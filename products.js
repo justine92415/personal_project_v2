2ac2d72bb1747ba0cab3f3792c488079451cae3f
justine92415 <justine92415@gmail.com>
2022-10-17 17:22:33 +0800

    test SPA

--- products.js
+++ products.js
@@ -1,16 +1,20 @@
 const rowBody = document.querySelector(".row-body");
-console.log(rowBody);
 
-const url =
-    "https://learnnodejs-3s6rmmfxwq-de.a.run.app/api/v1/tours/?category=raw";
+const categoryItem = document.querySelectorAll(".category-item");
+
+let currentUrl = document.URL;
+let curUrl = new URL(currentUrl);
+console.log(curUrl.searchParams.toString());
+const url = `https://learnnodejs-3s6rmmfxwq-de.a.run.app/api/v1/tours/?category${
+    curUrl.searchParams.toString() || "=raw"
+}`;
+
+console.log(curUrl.searchParams.toString());
 
 axios
     .get(url)
     .then(function (res) {
         tourData = res.data.dtat.tours;
-
-        console.log(tourData);
-        console.log(Array.isArray(tourData));
 
         tourData.forEach((choco) => {
             rowBody.innerHTML += `<div class="col-4">
@@ -52,7 +56,53 @@ axios
         });
     })
     .then(function () {
-        console.log("step2");
         const getBtn = document.querySelector(".carts__btn");
-        console.log(getBtn);
     });
+
+categoryItem.forEach((category) => {
+    category.addEventListener("click", (e) => {
+        history.replaceState(null, null, `?=${e.target.dataset.category}`);
+        let queryUrl = `https://learnnodejs-3s6rmmfxwq-de.a.run.app/api/v1/tours/?category=${e.target.dataset.category}`;
+        rowBody.innerHTML = "";
+        axios.get(queryUrl).then(function (res) {
+            res.data.dtat.tours.forEach((choco) => {
+                rowBody.innerHTML += `<div class="col-4">
+            <div class="product-card">
+                <div class="product-img">
+                    <div class="hovershow ">
+                        <p class="hovershow__name">${choco.name}</p>
+                        <span class="hovershow__div"></span>
+                        <p class="hovershow__price">NT.300</p>
+                        <p class="hovershow__description">
+                        ${choco.description}
+                        </p>
+                        <div class="hovershow__carts">
+                            <button class="carts__btn">
+                                <ion-icon class="btn__plus" name="remove-outline"></ion-icon>
+                            </button>
+                            <input class="carts__num" type="number" min="1" max="99" value="1">
+                            <button class="carts__btn">
+                                <ion-icon class="btn__minus" name="add-outline"></ion-icon>
+                            </button>
+                            <button class="carts__btn">
+                                <ion-icon class="btn__carts" name="cart-outline"></ion-icon>
+                            </button>
+                        </div>
+                    </div>
+                    <img src="./assets/chocoproducts/${choco.images[0]}">
+            
+                </div>
+            
+                <div class="product-content">
+                    <div class="viewlink">
+                        <a href="single.html">詳細介紹</a>
+                    </div>
+                    <div class="product-name">${choco.name}</div>
+                    <div class="product-price">NT.${choco.price}</div>
+                </div>
+            </div>
+            </div>`;
+            });
+        });
+    });
+});
