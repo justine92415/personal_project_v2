fafb026139108bcd315a6880a2c60490cabfe814
justine92415 <justine92415@gmail.com>
2022-10-18 21:29:35 +0800

    update page and sort function

--- products_V2.js
+++ products_V2.js
@@ -1,58 +1,99 @@
-// 獲取產品卡片容器
 const rowBody = document.querySelector(".row-body");
-// 獲取所有產品分類
 const categoryItem = document.querySelectorAll(".category-item");
-// 獲取頁碼容器
 const pagination = document.querySelector(".pagination");
-// 獲取當前的路徑
+const productsTotal = document.querySelector(".products-total");
+const sortType = this.document.querySelector(".sort-type");
+
 let curUrl = new URL(document.URL);
-// 當前路徑的查詢參數
 let queryParam = curUrl.searchParams.toString();
-// 請求路徑，當沒有查詢參數時預設取得所有產品數據，若有查詢參數時則獲取該產品數據
-let url = `https://learnnodejs-3s6rmmfxwq-de.a.run.app/api/v1/tours/${
-    queryParam === "" ? "?sort=name&limit=6&page=1" : "?" + queryParam
+let apiURL = "https://learnnodejs-3s6rmmfxwq-de.a.run.app/api/v1/tours/";
+let defaultSort = "name";
+
+let url = `${apiURL}${
+    queryParam === "" ? `?sort=${defaultSort}&limit=6&page=1` : "?" + queryParam
 }`;
-console.log(history.state);
-// 向Server取得產品數據
+
+// let hstate = history.state;
+if (!history.state) {
+    getPageNum();
+} else {
+    getPageNum(history.state.category);
+}
+
 getAjaxData(url);
 
-// 遍歷所有產品分類
-categoryItem.forEach((category) => {
-    // 為每個產品分類建立事件聆聽(點擊事件)
-    category.addEventListener("click", (e) => {
-        // 取得點擊目標的category屬性
-        let curCategory = e.target.dataset.category;
-        // 當前的curCategory若為all則向Server取得所有產品資料，否則只取某產品的資料
-        if (curCategory === "all") {
-            history.replaceState(null, null, `/products.html`);
-            url = `https://learnnodejs-3s6rmmfxwq-de.a.run.app/api/v1/tours/?sort=name&limit=6&page=1`;
-            getAjaxData(url);
-            // 取得所有頁碼
-            getPageNum();
-        } else {
-            history.replaceState(
-                { category: curCategory, sort: "name", limit: 6, page: 1 },
-                null,
-                `?category=${curCategory}&sort=name&limit=6&page=1`
-            );
-            url = `https://learnnodejs-3s6rmmfxwq-de.a.run.app/api/v1/tours/?category=${e.target.dataset.category}&sort=name&limit=6&page=1`;
-
-            getAjaxData(url);
-            getPageNum(curCategory);
-        }
-    });
+sortType.addEventListener("change", (e) => {
+    defaultSort = e.target.value;
+    console.log(location.search);
+    if (location.search.split("&").length === 3 || !location.search) {
+        url = `${apiURL}?sort=${defaultSort}&limit=6&page=${
+            history.state ? history.state.page : "1"
+        }`;
+        history.pushState(
+            null,
+            null,
+            `?sort=${defaultSort}&limit=6&page=${
+                history.state ? history.state.page : "1"
+            }`
+        );
+        getAjaxData(url);
+    } else {
+    }
 });
+console.log(defaultSort);
 
-// 上、下頁功能，當使用者點擊上下頁按鈕時觸發事件
 window.addEventListener("popstate", (e) => {
     // 取得目前網址列中的查詢參數
     queryParam = e.target.location.search;
 
     // 查詢參數為空時則取得所有產品資料，存在查詢參數時則取得該產品資料
-    let url = `https://learnnodejs-3s6rmmfxwq-de.a.run.app/api/v1/tours/${
-        queryParam === "" ? "?sort=name&limit=6&page=1" : queryParam
+    let url = `${apiURL}${
+        queryParam === "" ? `?sort=${defaultSort}&limit=6&page=1` : queryParam
     }`;
     getAjaxData(url);
+    let pageItems = document.querySelectorAll(".page-numbers");
+    pageItems.forEach((pageItem) => {
+        pageItem.classList.remove("current");
+    });
+    if (!history.state) {
+        // pageItems[0].classList.add("current");
+        getPageNum();
+    } else {
+        // let historyPage = history.state.page;
+        getPageNum(history.state.category);
+    }
+
+    rowBody.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest",
+    });
+});
+
+categoryItem.forEach((category) => {
+    // 為每個產品分類建立事件聆聽(點擊事件)
+    category.addEventListener("click", (e) => {
+        // 取得點擊目標的category屬性
+        let curCategory = e.target.dataset.category;
+        defaultSort = "name";
+        // 當前的curCategory若為all則向Server取得所有產品資料，否則只取某產品的資料
+        if (curCategory === "all") {
+            history.pushState(null, null, `/products.html`);
+            url = `${apiURL}?sort=${defaultSort}&limit=6&page=1`;
+            getAjaxData(url);
+            // 取得所有頁碼
+            getPageNum();
+        } else {
+            history.pushState(
+                { category: curCategory, sort: "name", limit: 6, page: 1 },
+                null,
+                `?category=${curCategory}&sort=${defaultSort}&limit=6&page=1`
+            );
+            url = `${apiURL}?category=${curCategory}&sort=${defaultSort}&limit=6&page=1`;
+            getAjaxData(url);
+            getPageNum(curCategory);
+        }
+    });
 });
 
 function getAjaxData(reqUrl) {
@@ -104,18 +145,14 @@ function getAjaxData(reqUrl) {
 
 function getPageNum(category = "") {
     pagination.innerHTML = "";
-    // console.log(category);
     axios
-        .get(
-            `https://learnnodejs-3s6rmmfxwq-de.a.run.app/api/v1/tours/${
-                category ? "?category=" + category : ""
-            }`
-        )
+        .get(`${apiURL}${category ? "?category=" + category : ""}`)
         .then(function (res) {
             let refPage = +location.search.slice(-1);
             let { tours } = res.data.dtat;
-            // console.log(tours);
             let totalPage = Math.ceil(tours.length / 6);
+
+            productsTotal.innerText = `共${tours.length}件商品`;
             for (let i = 0; i < totalPage; i++) {
                 pagination.innerHTML += ` 
                     <li class="page-numbers" data-page=${i + 1}>
@@ -134,17 +171,41 @@ function getPageNum(category = "") {
             // console.log(window.location.search);
             pageItems.forEach((pageItem) => {
                 pageItem.addEventListener("click", (e) => {
+                    let curPage = e.target.dataset.page;
                     for (let i = 0; i < pageItems.length; i++) {
                         pageItems[i].classList.remove("current");
                     }
                     e.target.classList.add("current");
-                    let curPage = e.target.dataset.page;
-                    url = `https://learnnodejs-3s6rmmfxwq-de.a.run.app/api/v1/tours/?sort=name&limit=6&page=${curPage}`;
-                    history.pushState(
-                        null,
-                        null,
-                        `?sort=name&limit=6&page=${curPage}`
-                    );
+
+                    if (
+                        location.search.split("&").length === 3 ||
+                        !location.search
+                    ) {
+                        url = `${apiURL}?sort=${defaultSort}&limit=6&page=${curPage}`;
+                        history.pushState(
+                            {
+                                category: "",
+                                sort: defaultSort,
+                                limit: 6,
+                                page: curPage,
+                            },
+                            null,
+                            `?sort=${defaultSort}&limit=6&page=${curPage}`
+                        );
+                    } else {
+                        url = `${apiURL}?category=${category}&sort=${defaultSort}&limit=6&page=${curPage}`;
+                        history.pushState(
+                            {
+                                category,
+                                sort: defaultSort,
+                                limit: 6,
+                                page: curPage,
+                            },
+                            null,
+                            `?category=${category}&sort=${defaultSort}&limit=6&page=${curPage}`
+                        );
+                    }
+
                     getAjaxData(url);
 
                     rowBody.scrollIntoView({
@@ -156,4 +217,3 @@ function getPageNum(category = "") {
             });
         });
 }
-getPageNum();
